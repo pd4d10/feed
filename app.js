a988c5f32b8b1ab6e7255b335044b6d5d3132f36
pd4d10 <pd4d10@gmail.com>
2017-09-21 20:30:21 +0800

    Add zhihu zhuanlan implement

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const Koa = require('koa')
 const KoaRouter = require('koa-router')
+const Raven = require('raven')
 const fetch = require('node-fetch')
 const Feed = require('feed')
 const marked = require('marked')
 
+Raven.config(
+  'https://1806e37bad074b9386873d165d9f8ce3:5db2f86872464237b1afb55d28810697@sentry.io/220223'
+).install()
+
 const app = new Koa()
 const router = new KoaRouter()
 
+const PAGE_SIZE = 20 // 20 items for every pull
+
 router.get('/github-issue/:owner/:repo', async ctx => {
   const { owner, repo } = ctx.params
-  const res = await fetch(
-    `https://api.github.com/repos/${owner}/${repo}/issues?state=all`
-  )
+  const api = `https://api.github.com/repos/${owner}/${repo}/issues?state=all&creator=${owner}`
+  const res = await fetch(api)
   const issues = await res.json()
   // const issues = require('./data')
 
@@ -46,9 +52,42 @@ router.get('/github-issue/:owner/:repo', async ctx => {
   ctx.body = feed.atom1()
 })
 
-router.get('/zhihu-zhuanlan/', async ctx => {
-  ctx.body = 'Hello'
+router.get('/zhihu-zhuanlan/:name', async ctx => {
+  const { name } = ctx.params
+  const api = `https://zhuanlan.zhihu.com/api/columns/${name}/posts?limit=${PAGE_SIZE}`
+  const res = await fetch(api)
+  const posts = await res.json()
+
+  const feed = new Feed({
+    title: `${name} - 知乎专栏`,
+    description: `${name} - 知乎专栏`,
+    id: name,
+    link: `https://zhuanlan.zhihu.com/${name}`,
+  })
+
+  posts.forEach(post => {
+    feed.addItem({
+      title: post.title,
+      id: post.slug,
+      link: `https://zhuanlan.zhihu.com${post.url}`,
+      description: post.summary,
+      content: post.content,
+      author: [
+        {
+          name: post.author.name,
+          link: post.author.profileUrl,
+        },
+      ],
+      date: new Date(post.publishedTime),
+      image: post.titleImage,
+    })
+  })
+
+  ctx.set('Content-Type', 'application/atom+xml')
+  ctx.body = feed.atom1()
 })
+
+// router.get('/', )
 
 app.use(router.routes()).use(router.allowedMethods())
 
